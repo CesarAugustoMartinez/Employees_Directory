027aa17d91ff66f81378e389e2fc6051c5451ffc
Cesar Martinez <cesaguma@hotmail.com>
2021-01-16 02:02:49 -0500

    Deploying

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Employees from "./pages/Employees";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <Router>
+    // <Router>
       <div>
         <Wrapper>
-          <Route exact path="/" component={Employees} />
+          <Employees />
         </Wrapper>
         <Footer />
       </div>
-    </Router>
+    // </Router>
   );
 }
 
